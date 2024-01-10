8ef5a7aeaf9e3173d2a0806410cc65174800c683
A253875 <A253875@LSBSA-65LG3J3>
2024-01-10 13:45:31 +0200

    Add project files.

---
+++ 12.If Statement 2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12.If_Statement_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GetMax(2, 10));
+            Console.ReadLine();
+
+        }
+        //method returns the biggest number out of the two numbers 
+        static int GetMax(int num1, int num2)
+        {
+            if (num1 > num2)
+            {
+                return num1;
+            }
+            else
+            {
+                return num2;
+            }
+        }
+    }
+}
